e364fa844c07bf351a45a43c0b2bbf3a23058af4
Syukri Lajin <ginger_boy92@hotmail.com>
2014-09-08 06:30:13 +0800

    mkvendor.sh: Initial commit of note

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := note
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/note/device_note.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := note
+PRODUCT_NAME := cm_note
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := note
+PRODUCT_MANUFACTURER := xiaomi
